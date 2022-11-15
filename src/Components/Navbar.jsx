58a5f9b6dbd7dff748c1125594cd5f23629771ad
Jesse Ge <71725877+jesse1337@users.noreply.github.com>
2022-11-15 11:48:50 -0800

    Add files via upload

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,8 +7,8 @@ export default function Navbar() {
     return (
         <nav className="nav">
             <ul className="nav-items">
-                <li>Gallery</li>
-                <li>Socials</li>
+                <li><a href="https://github.com/jesse1337" target="_blank">Github</a></li>
+                <li><a href="https://www.linkedin.com/in/jesse-ge/" target="_blank">LinkedIn</a></li>
             </ul>
         </nav>
 
